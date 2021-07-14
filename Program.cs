500682b29d8a8d3a79fcfff44ab173a512d7e954
Alex Vogt <alexvogt18@gmail.com>
2021-07-14 16:58:48 -0700

    Adds cs files to repo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab01_Numbers_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
